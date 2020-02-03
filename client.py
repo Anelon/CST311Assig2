c7d747c1540a41b0899827c902b965cce0651866
DuskEcho <42277980+DuskEcho@users.noreply.github.com>
2020-02-03 15:49:22 -0800

    Update client.py

--- client.py
+++ client.py
@@ -1,8 +1,8 @@
-# The client sends a “ping” message package to the UDP server. Then UDP client waits for a second 
+# The client sends a “ping” message package to the UDP server. Then UDP client waits for a second
 # before retransmissions/sends another “ping” message package to the UDP server and it blocks the
 # income “ping” message package. If it the UDP client has to wait longer than 1 second to
 # retransmissions/resends another package, then this would result in loss of packages because the
-# packages that were sent did not get sent back. This program calculates and prints the minimum, 
+# packages that were sent did not get sent back. This program calculates and prints the minimum,
 # maximum, and average RTTs at the end of all pings from the client, the number of packets lost
 # and the packet loss rate (in percentage).  It also computes and prints the estimated RTT,
 # DevRTT, and the TimeoutInterval based on the RTT results.
@@ -11,6 +11,7 @@ from __future__ import division #uses division format
 from socket import *  # import socket interfaces
 from time import  time
 from statistics import mean
+from datetime import datetime
 serverName = 'localhost'  # server ip for computers
 serverPort = 12000 # server port number
 clientSocket = socket(AF_INET, SOCK_DGRAM)
@@ -34,13 +35,13 @@ address = serverName
 while sequence_number < 11:
     sequence_number +=1
     message = "Ping"
-    start_time = int(round(time() * 1000)) # current start time
+    start_time = datetime.now().microsecond/1000
     # the client sends the message to the server, the client sends the ping message to the server
     clientSocket.sendto(message.encode(), (serverName, serverPort))
 
     try:
         message, address = clientSocket.recvfrom(1024) # message and address is received from the server, the client gets ping back
-        end_time = int(round(time() * 1000)) #end time
+        end_time = datetime.now().microsecond/1000  #end time
         SampleRTT = end_time-start_time #round trip time for the UPD client to send “ping” and get the “ping” message back
         print("Server responded: Round trip time (RTT) =", SampleRTT)
         if minRTT == -1:
@@ -77,12 +78,13 @@ while sequence_number < 11:
         lostPackages +=1
 
         #package lost percentages =( packets lost)/(# packet sent).
-        percentage =  "{0:.0f}%".format((lostPackages/sequence_number)*100)
+        percentage = "{0:.0f}%".format((lostPackages/sequence_number)*100)
         print("Ping statistics for", address)
         print("Packets: Sent =", sequence_number, "Received =", sequence_number, "Lost = ", lostPackages, "(", percentage, "loss),")
     finally:
         if sequence_number == 11 :
             clientSocket.close()  #closes connection
+            percentage = "{0:.0f}%".format((lostPackages/sequence_number)*100)
 
 
 
